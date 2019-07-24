048918516e3322b522eb57ad21bc50505bbe96b8
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 18:01:08 -0700

    Add files via upload

---
+++ src/main/main.swift
@@ -0,0 +1,27 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.swift                                                                *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Swift' is desplayed to the standerd console.
+ *
+ */
+func main()
+{
+    print("Hello, Swift.");
+    
+}// end of function main
+
+main()
